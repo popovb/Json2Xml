4d743ea13f8eb6159a0ed91a70a76d721573ea27
Boris Popov <popov@whitekefir.ru>
2017-03-27 11:51:12 +0300

    add dumb for Option

---
+++ include/Json2Xml/Option.hpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef __Option_hpp__
+#define __Option_hpp__
+
+namespace json2xml {
+//////////////////////////////////////////////////////////////////
+     class Option {
+     };
+//////////////////////////////////////////////////////////////////
+}
+#endif // __Option_hpp__
