ec09c7696c01eaca80e70dc303410fa6bde68fb9
Boris Popov <popov@whitekefir.ru>
2017-03-27 13:23:15 +0300

    add impl for Option

--- include/Json2Xml/Option.hpp
+++ include/Json2Xml/Option.hpp
@@ -6,12 +6,34 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
+#include "String.hpp"
+
 #ifndef __Option_hpp__
 #define __Option_hpp__
 
 namespace json2xml {
 //////////////////////////////////////////////////////////////////
+     using RootName        = String;
+     using ArraysItemName  = String;
+     using ArraysCountName = String;
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
      class Option {
+
+     public:
+	  Option(const RootName        = "json",
+		 const ArraysItemName  = "item",
+		 const ArraysCountName = "n");
+
+	  RootName getRootName() const;
+	  ArraysItemName getArraysItemName() const;
+	  ArraysCountName getArraysCountName() const;
+
+     private:
+	  const RootName rn;
+	  const ArraysItemName ain;
+	  const ArraysCountName acn;
      };
 //////////////////////////////////////////////////////////////////
 }
---
+++ src/Option.cpp
@@ -0,0 +1,35 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include <Json2Xml/Option.hpp>
+
+//////////////////////////////////////////////////////////////////
+json2xml::Option::Option(const RootName n,
+			 const ArraysItemName in,
+			 const ArraysCountName cn):
+     rn(n),
+     ain(in),
+     acn(cn)
+{
+     return;
+}
+
+json2xml::RootName json2xml::Option::getRootName() const {
+     return rn;
+}
+
+json2xml::ArraysItemName
+json2xml::Option::getArraysItemName() const {
+     return ain;
+}
+
+json2xml::ArraysCountName
+json2xml::Option::getArraysCountName() const {
+     return acn;
+}
+//////////////////////////////////////////////////////////////////
