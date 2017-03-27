99845700bb03fe723a6136e9ab6261eba65e0925
Boris Popov <popov@whitekefir.ru>
2017-03-27 13:24:47 +0300

    add String

---
+++ include/Json2Xml/String.hpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include <string>
+
+#ifndef __String_hpp__
+#define __String_hpp__
+
+namespace json2xml {
+//////////////////////////////////////////////////////////////////
+     using String = std::string;
+//////////////////////////////////////////////////////////////////
+}
+#endif // __String_hpp__
