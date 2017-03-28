4ff8a83649c7ed765c38849c86780463989a24df
Boris Popov <popov@whitekefir.ru>
2017-03-28 16:12:28 +0300

    begin proto for ConvertHandler

---
+++ src/ConvertHandler.hpp
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include <Json2Xml/Option.hpp>
+#include <Json2Xml/Handler.hpp>
+#include "SimpleJson/Handler.hpp"
+#include "SimpleJson/Common.hpp"
+
+#ifndef __json2xml_ConvertHandler_hpp__
+#define __json2xml_ConvertHandler_hpp__
+
+namespace json2xml {
+//////////////////////////////////////////////////////////////////
+     class ConvertHandler: public simple_json::Handler {
+
+     public:
+	  ConvertHandler(::json2xml::Handler&, const Option&);
+
+	  virtual ~ConvertHandler();
+
+	  virtual void ObjectStart();
+	  virtual void ObjectEnd();
+
+	  virtual void ArrayStart();
+	  virtual void ArrayEnd();
+
+	  virtual void Key(const simple_json::key_t);
+	  virtual void Value(const simple_json::value_t);
+     };
+//////////////////////////////////////////////////////////////////
+}
+#endif // __json2xml_ConvertHandler_hpp__
