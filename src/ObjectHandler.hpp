c5a7b508563e08730bc90cef84c0eda2566716e4
Boris Popov <popov@whitekefir.ru>
2017-04-03 17:01:03 +0300

    add proto for ObjectHandler

---
+++ src/ObjectHandler.hpp
@@ -0,0 +1,32 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include "TagHistory.hpp"
+#include "PlaceLooker.hpp"
+#include "Instructions.hpp"
+#include <memory>
+
+#ifndef __json2xml_ObjectHandler_hpp__
+#define __json2xml_ObjectHandler_hpp__
+
+namespace json2xml {
+//////////////////////////////////////////////////////////////////
+     class _ObjectHandler {
+
+     public:
+	  virtual ~_ObjectHandler() = 0;
+
+	  Instructions handle(TagHistory&, PlaceLooker&) const;
+     };
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+     using ObjectHandler = std::unique_ptr<_ObjectHandler>;
+//////////////////////////////////////////////////////////////////
+}
+#endif // __json2xml_ObjectHandler_hpp__
