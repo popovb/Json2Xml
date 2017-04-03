79f33a3e0b99b41e27ff4bf26a016d06d340e9cb
Boris Popov <popov@whitekefir.ru>
2017-04-03 17:00:34 +0300

    add proto for ObjectHandlerFabric

---
+++ src/ObjectHandlerFabric.hpp
@@ -0,0 +1,26 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include <Json2Xml/Option.hpp>
+#include "ObjectHandler.hpp"
+
+#ifndef __json2xml_ObjectHandlerFabric_hpp__
+#define __json2xml_ObjectHandlerFabric_hpp__
+
+namespace json2xml {
+//////////////////////////////////////////////////////////////////
+     class ObjectHandlerFabric {
+
+     public:
+	  ObjectHandlerFabric(const Option&);
+
+	  ObjectHandler make(EventLooker&) const;
+     };
+//////////////////////////////////////////////////////////////////
+}
+#endif // __json2xml_ObjectHandlerFabric_hpp__
