5786685bca8db20e7ebbec4906e3a29959580a2d
Boris Popov <popov@whitekefir.ru>
2017-04-04 13:49:01 +0300

    add proto for KeyHandler

---
+++ src/KeyHandler.hpp
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include "ObjectHandler.hpp"
+#include "Instructions.hpp"
+
+#ifndef __json2xml_KeyHandler_hpp__
+#define __json2xml_KeyHandler_hpp__
+
+namespace json2xml {
+//////////////////////////////////////////////////////////////////
+     class KeyHandler: public _ObjectHandler {
+
+     public:
+	  virtual ~KeyHandler();
+
+	  virtual Instructions handle(TagHistory&,
+				      PlaceLooker&) const override;
+     };
+//////////////////////////////////////////////////////////////////
+}
+#endif // __json2xml_KeyHandler_hpp__
