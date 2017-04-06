5671ca0e1ae7a8d673f2747a4af807b5b73b7a4b
Boris Popov <popov@whitekefir.ru>
2017-04-06 11:35:37 +0300

    add unit test for ObjectStartHandler

--- tests/objectstarthandler_test.cpp
+++ tests/objectstarthandler_test.cpp
@@ -5,11 +5,32 @@ TEST(objectstarthandler, test_01)
 {
      //
      // { }
-     //
+     // ^
      using namespace json2xml;
-     ObjectStartHandler OSH(Option(), Event::UNDEF);
+     Option O;
+     ObjectStartHandler OSH(O, Event::UNDEF);
      TagHistory TH("json");
      PlaceLooker PL;
+     PL++;
+     PL.set_object();
+     auto is = OSH.handle(TH, PL);
+     ASSERT_EQ(1, is.size());
+     Instruction i1({ InstType::OPEN, {"Json"} });
+     ASSERT_EQ(i1, is[0]);
+}
+
+TEST(objectstarthandler, test_02)
+{
+     //
+     // { { } }
+     //   ^
+     using namespace json2xml;
+     Option O;
+     ObjectStartHandler OSH(O, Event::OBJECTSTART);
+     TagHistory TH("json");
+     PlaceLooker PL;
+     PL++;
+     PL.set_object();
      PL++;
      PL.set_object();
      auto is = OSH.handle(TH, PL);
