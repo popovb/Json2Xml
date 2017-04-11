03c409b101df779bade4b602e7b58ccb139100a7
Boris Popov <popov@whitekefir.ru>
2017-04-11 13:48:55 +0300

    add impl for ArrayStartHandler

--- src/ArrayStartHandler.cpp
+++ src/ArrayStartHandler.cpp
@@ -10,20 +10,352 @@
 
 //////////////////////////////////////////////////////////////////
 json2xml::
-ArrayStartHandler::ArrayStartHandler(const Option& o):
-     option(o)
+ArrayStartHandler::ArrayStartHandler(const Option& o,
+				     const Event e):
+     option(o),
+     previous(e)
 {
      return;
 }
 
 json2xml::Instructions
-json2xml::ArrayStartHandler::handle(TagHistory&,
-				    PlaceLooker&,
-				    const Argument) const {
-     //
-     //TODO
-     //
-     Instructions i;
-     return i;
+json2xml::ArrayStartHandler::handle(TagHistory& th,
+				    PlaceLooker& pl,
+				    const Argument a) const {
+     switch (previous) {
+     case Event::UNDEF:
+	  return prev_undef(th, pl);
+	  
+     case Event::OBJECTSTART:
+	  return prev_objectstart(th, pl);
+	  
+     case Event::OBJECTEND:
+	  return prev_objectend(th, pl);
+	  
+     case Event::ARRAYSTART:
+	  return prev_arraystart(th, pl);
+	  
+     case Event::ARRAYEND:
+	  return prev_arrayend(th, pl);
+	  
+     case Event::KEY:
+	  return prev_key(th, pl, a);
+	  
+     case Event::VALUE:
+	  return prev_value(th, pl, a);
+
+     default:
+	  return dflt(th, pl);
+     }
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_undef (TagHistory& th, const PlaceLooker& pl) const {
+     if (pl.is_object()) {
+	  return prev_undef_in_object(th);
+	  
+     } else if (pl.is_array()) {
+	  return prev_undef_in_array(th);
+
+     } else {
+	  return prev_undef_(th);
+     }
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_objectstart (TagHistory& th, const PlaceLooker& pl) const {
+     if (pl.is_object()) {
+	  return prev_objectstart_in_object(th);
+	  
+     } else if (pl.is_array()) {
+	  return prev_objectstart_in_array(th);
+
+     } else {
+	  return prev_objectstart_(th);
+     }
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_objectend(TagHistory& th, const PlaceLooker& pl) const {
+     if (pl.is_object()) {
+	  return prev_objectend_in_object(th);
+	  
+     } else if (pl.is_array()) {
+	  return prev_objectend_in_array(th, pl.get_count());
+
+     } else {
+	  return prev_objectend_(th);
+     }
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_arraystart(TagHistory& th, const PlaceLooker& pl) const {
+     if (pl.is_object()) {
+	  return prev_arraystart_in_object(th);
+	  
+     } else if (pl.is_array()) {
+	  return prev_arraystart_in_array(th, pl.get_count());
+
+     } else {
+	  return prev_arraystart_(th);
+     }
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_arrayend(TagHistory& th, const PlaceLooker& pl) const {
+     if (pl.is_object()) {
+	  return prev_arrayend_in_object(th);
+	  
+     } else if (pl.is_array()) {
+	  return prev_arrayend_in_array(th, pl.get_count());
+
+     } else {
+	  return prev_arrayend_(th);
+     }
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_key(TagHistory& th, const PlaceLooker& pl,
+	 const Argument a) const {
+     if (pl.is_object()) {
+	  return prev_key_in_object(th, a);
+	  
+     } else if (pl.is_array()) {
+	  return prev_key_in_array(th, a);
+
+     } else {
+	  return prev_key_(th, a);
+     }
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_value(TagHistory& th, const PlaceLooker& pl,
+	   const Argument a) const {
+     if (pl.is_object()) {
+	  return prev_value_in_object(th, a);
+	  
+     } else if (pl.is_array()) {
+	  return prev_value_in_array(th, a, pl.get_count());
+
+     } else {
+	  return prev_value_(th, a);
+     }
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+dflt(TagHistory&, const PlaceLooker&) const {
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_undef_in_object(TagHistory& th) const {
+     Instructions is;
+     // Instruction i({ InstType::OPEN, { option.getRootName() } });
+     // is.push_back(i);
+     // th.push(option.getRootName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_undef_in_array(TagHistory&) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
+}
+     
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_undef_(TagHistory& th) const {
+     Instructions is;
+     // Instruction i({ InstType::OPEN, { option.getRootName() } });
+     // is.push_back(i);
+     // th.push(option.getRootName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_objectstart_in_object(TagHistory& th) const {
+     Instructions is;
+     // Instruction i({ InstType::OPEN, { option.getDefaultName() } });
+     // is.push_back(i);
+     // th.push(option.getDefaultName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_objectstart_in_array(TagHistory&) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_objectstart_(TagHistory&) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_objectend_in_object(TagHistory& th) const {
+     Instructions is;
+     // Instruction i({ InstType::OPEN, { option.getDefaultName() } });
+     // is.push_back(i);
+     // th.push(option.getDefaultName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::   
+prev_objectend_in_array(TagHistory& th,
+			const PlaceLooker::count_t i) const {
+     Instructions is;
+     // Instruction i1({ InstType::OPEN, { option.getArraysItemName() } });
+     // Instruction i2({ InstType::AV, { option.getArraysCountName(),
+     // 				      std::to_string(i) } });
+     // is.push_back(i1);
+     // is.push_back(i2);
+     // th.push(option.getArraysItemName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::   
+prev_objectend_(TagHistory&) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::   
+prev_arraystart_in_object(TagHistory&) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::        
+prev_arraystart_in_array(TagHistory& th,
+			 const PlaceLooker::count_t i) const {
+     Instructions is;
+     // Instruction i1({ InstType::OPEN, { option.getArraysItemName() } });
+     // Instruction i2({ InstType::AV, { option.getArraysCountName(),
+     // 				      std::to_string(i) } });
+     // is.push_back(i1);
+     // is.push_back(i2);
+     // th.push(option.getArraysItemName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_arraystart_(TagHistory&) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_arrayend_in_object(TagHistory& th) const {
+     Instructions is;
+     // Instruction i({ InstType::OPEN, { option.getDefaultName() } });
+     // is.push_back(i);
+     // th.push(option.getDefaultName());
+     return is;
+}
+     
+json2xml::Instructions json2xml::ArrayStartHandler::
+prev_arrayend_in_array(TagHistory& th,
+		       const PlaceLooker::count_t i) const {
+     Instructions is;
+     // Instruction i1({ InstType::OPEN, { option.getArraysItemName() } });
+     // Instruction i2({ InstType::AV, { option.getArraysCountName(),
+     // 				      std::to_string(i) } });
+     // is.push_back(i1);
+     // is.push_back(i2);
+     // th.push(option.getArraysItemName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::     
+prev_arrayend_(TagHistory&) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::     
+prev_key_in_object(TagHistory&,
+		   const Argument) const {
+     // //
+     // //TODO
+     // //
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::          
+prev_key_in_array(TagHistory&,
+		  const Argument) const {
+     // //
+     // //TODO
+     // //
+     Instructions is;
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::          
+prev_key_(TagHistory&, const
+	  Argument) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
+}
+     
+json2xml::Instructions json2xml::ArrayStartHandler::          
+prev_value_in_object(TagHistory& th,
+		     const Argument) const {
+     Instructions is;
+     // Instruction i({ InstType::OPEN, { option.getDefaultName() } });
+     // is.push_back(i);
+     // th.push(option.getDefaultName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::               
+prev_value_in_array(TagHistory& th,
+		    const Argument,
+		    const PlaceLooker::count_t i) const {
+     Instructions is;
+     // Instruction i1({ InstType::OPEN, { option.getArraysItemName() } });
+     // Instruction i2({ InstType::AV, { option.getArraysCountName(),
+     // 				      std::to_string(i) } });
+     // is.push_back(i1);
+     // is.push_back(i2);
+     // th.push(option.getArraysItemName());
+     return is;
+}
+
+json2xml::Instructions json2xml::ArrayStartHandler::                    
+prev_value_(TagHistory&,
+	    const Argument) const {
+     // //
+     // //NOT SUPPORT
+     // //
+     Instructions is;
+     return is;
 }
 //////////////////////////////////////////////////////////////////
--- src/ArrayStartHandler.hpp
+++ src/ArrayStartHandler.hpp
@@ -8,6 +8,7 @@
 
 #include "ObjectHandler.hpp"
 #include "Instructions.hpp"
+#include "EventLooker.hpp"
 #include <Json2Xml/Option.hpp>
 
 #ifndef __json2xml_ArrayStartHandler_hpp__
@@ -18,13 +19,75 @@ namespace json2xml {
      class ArrayStartHandler: public _ObjectHandler {
 
      public:
-	  ArrayStartHandler(const Option&);
+	  ArrayStartHandler(const Option&,
+			    const Event);
 
-	  virtual Instructions handle(TagHistory&,
-				      PlaceLooker&,
-				      const Argument = "") const override;
+	  Instructions handle(TagHistory&,
+			      PlaceLooker&,
+			      const Argument = "") const override;
      private:
 	  const Option& option;
+	  const Event previous;
+
+	  Instructions prev_undef(TagHistory&,
+				  const PlaceLooker&) const;
+	  Instructions prev_objectstart(TagHistory&,
+					const PlaceLooker&) const;
+	  Instructions prev_objectend(TagHistory&,
+				      const PlaceLooker&) const;
+	  Instructions prev_arraystart(TagHistory&,
+				       const PlaceLooker&) const;
+	  Instructions prev_arrayend(TagHistory&,
+				     const PlaceLooker&) const;
+	  Instructions prev_key(TagHistory&,
+				const PlaceLooker&,
+				const Argument) const;
+	  Instructions prev_value(TagHistory&,
+				  const PlaceLooker&,
+				  const Argument) const;
+	  Instructions dflt(TagHistory&, const PlaceLooker&) const;
+
+	  Instructions prev_undef_in_object(TagHistory&) const;
+	  Instructions prev_undef_in_array(TagHistory&) const;
+	  Instructions prev_undef_(TagHistory&) const;
+
+	  Instructions prev_objectstart_in_object(TagHistory&) const;
+	  Instructions prev_objectstart_in_array (TagHistory&) const;
+	  Instructions prev_objectstart_(TagHistory&) const;
+
+	  Instructions prev_objectend_in_object(TagHistory&) const;
+	  Instructions
+	  prev_objectend_in_array(TagHistory&,
+				  const PlaceLooker::count_t) const;
+	  Instructions prev_objectend_(TagHistory&) const;
+
+	  	  Instructions prev_arraystart_in_object(TagHistory&) const;
+	  Instructions
+	  prev_arraystart_in_array(TagHistory&,
+				   const PlaceLooker::count_t) const;
+	  Instructions prev_arraystart_(TagHistory&) const;
+
+	  Instructions prev_arrayend_in_object(TagHistory&) const;
+	  Instructions
+	  prev_arrayend_in_array(TagHistory&,
+				 const PlaceLooker::count_t) const;
+	  Instructions prev_arrayend_(TagHistory&) const;
+
+	  Instructions prev_key_in_object(TagHistory&,
+					  const Argument) const;
+	  Instructions prev_key_in_array(TagHistory&,
+					 const Argument) const;
+	  Instructions prev_key_(TagHistory&, const
+				 Argument) const;
+
+	  Instructions prev_value_in_object(TagHistory&,
+					    const Argument) const;
+	  Instructions
+	  prev_value_in_array(TagHistory&,
+			      const Argument,
+			      const PlaceLooker::count_t) const;
+	  Instructions prev_value_(TagHistory&,
+				   const Argument) const;
      };
 //////////////////////////////////////////////////////////////////
 }
