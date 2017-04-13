8f4b3bbf609a92489266535a0ad138b5b821bd1c
Boris Popov <popov@whitekefir.ru>
2017-04-13 11:56:27 +0300

    add unit test for ValueHandler

--- tests/valuehandler_test.cpp
+++ tests/valuehandler_test.cpp
@@ -93,3 +93,27 @@ TEST(valuehandler, test_04)
      ASSERT_EQ(i2, is[2]);
      ASSERT_EQ(i3, is[3]);
 }
+
+TEST(valuehandler, test_05)
+{
+     //
+     // { ...] "value"...
+     //        ^
+     using namespace json2xml;
+     Option O;
+     ValueHandler VH(O, Event::ARRAYEND);
+     TagHistory TH("json");
+     PlaceLooker PL;
+     PL.set_object();
+     PL++;
+     PL++;
+     PL++;
+     auto is = VH.handle(TH, PL, "005");
+     ASSERT_EQ(3, is.size());
+     Instruction i0({ InstType::OPEN, {"json"} });
+     Instruction i1({ InstType::TEXT, {"005"} });
+     Instruction i2({ InstType::CLOSE, {"json"} });
+     ASSERT_EQ(i0, is[0]);
+     ASSERT_EQ(i1, is[1]);
+     ASSERT_EQ(i2, is[2]);
+}
