eac53dc65f290ce837f8b733a0c324313183dae7
Boris Popov <popov@whitekefir.ru>
2017-04-13 16:18:18 +0300

    add unit test for Converter

--- tests/converter_test.cpp
+++ tests/converter_test.cpp
@@ -1,6 +1,7 @@
 #include <Json2Xml/Converter.hpp>
 #include <Json2Xml/Option.hpp>
 #include <Json2Xml/NullHandler.hpp>
+#include <Json2Xml/TestHandler.hpp>
 #include <fstream>
 #include <gtest/gtest.h>
 
@@ -28,4 +29,47 @@ TEST(converter, test_03)
      Converter C;
      NullHandler NH;
      ASSERT_TRUE(C.convert(in, NH));
+}
+
+TEST(converter, test_04)
+{
+     using namespace json2xml;
+     std::ifstream in("first_test_05.json");
+     Converter C;
+     TestHandler TH("\n");
+     ASSERT_TRUE(C.convert(in, TH));
+     std::string ETALON = R"-(O[Json]
+O[hello5]
+T[world5]
+C[hello5]
+O[t]
+T[TRUE]
+C[t]
+O[f]
+T[FALSE]
+C[f]
+O[n]
+T[NULL]
+C[n]
+O[i]
+T[123]
+C[i]
+O[pi]
+O[x]
+T[6]
+C[x]
+C[pi]
+O[pi2]
+O[item]
+A[n][1]
+T[x]
+C[item]
+O[item]
+A[n][2]
+T[6]
+C[item]
+C[pi2]
+C[Json]
+)-";
+     ASSERT_EQ(ETALON, TH.get());
 }
