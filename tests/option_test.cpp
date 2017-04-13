7cecccca73a5b1ffe7eb8178ee1bdd5490e2ecc6
Boris Popov <popov@whitekefir.ru>
2017-04-13 14:18:23 +0300

    fix unit test

--- tests/option_test.cpp
+++ tests/option_test.cpp
@@ -12,7 +12,7 @@ TEST(option, test_02)
 {
      using namespace json2xml;
      Option O;
-     ASSERT_EQ("json", O.getRootName());
+     ASSERT_EQ("Json", O.getRootName());
 }
 
 TEST(option, test_03)
@@ -27,13 +27,4 @@ TEST(option, test_04)
      using namespace json2xml;
      Option O;
      ASSERT_EQ("n", O.getArraysCountName());
-}
-
-TEST(option, test_05)
-{
-     using namespace json2xml;
-     Option O("json2", "item2", "i");
-     ASSERT_EQ("json2", O.getRootName());
-     ASSERT_EQ("item2", O.getArraysItemName());
-     ASSERT_EQ("i", O.getArraysCountName());
 }
