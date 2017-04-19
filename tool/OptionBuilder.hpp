e03d02f134d94b1724d5f5cb17d7898754b76ca1
Boris Popov <popov@whitekefir.ru>
2017-04-19 13:03:40 +0300

    add impl for OptionBuilder::build

--- tool/OptionBuilder.hpp
+++ tool/OptionBuilder.hpp
@@ -6,7 +6,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
-#include "Config.hpp"
 #include <Json2Xml/Option.hpp>
 
 #ifndef __OptionBuilder__
@@ -16,7 +15,24 @@
 class OptionBuilder {
      
 public:
-     json2xml::Option build(const Config&) const;
+     template <class CFG>
+     json2xml::Option build(const CFG& c) const {
+	  json2xml::Option o;
+	  auto tg = c.getTagNames();
+	  if (tg.count("root_name")) {
+	       o.setRootName(tg["root_name"]);
+	  }
+	  if (tg.count("default_name")) {
+	       o.setDefaultName(tg["default_name"]);
+	  }
+	  if (tg.count("arrays_item_name")) {
+	       o.setArraysItemName(tg["arrays_item_name"]);
+	  }
+	  if (tg.count("arrays_count_name")) {
+	       o.setArraysCountName(tg["arrays_count_name"]);
+	  }
+	  return o;
+     }
 };
 //////////////////////////////////////////////////////////////////
 #endif // __OptionBuilder__
